d6814b26b3b3dd5ac5ada903b88bc9c6d1649c42
why_code <119089018+VWiman@users.noreply.github.com>
2025-09-30 10:19:42 +0200

    Added comments and implemented logging.

--- app.py
+++ app.py
@@ -1,37 +1,60 @@
 import os
+import logging
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+# Configure logging
+logging.basicConfig(format='%(asctime)s %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p', encoding='utf-8', level=logging.DEBUG)
 
+app = Flask(__name__)
+logger = logging.getLogger(__name__)
+
+# Define Paths
 base_dir_path = os.path.dirname(os.path.realpath(__file__))
 downloads_dir_path = os.path.join(base_dir_path, 'data/downloads')
 
-def dir_to_list(dir: str):
-    dir_list = os.listdir(dir)
-    return dir_list
+# Ensure downloads directory exists
+def ensure_folder(folder: str) -> None:
+    try:
+        os.makedirs(folder, exist_ok=True)
+        logger.info(f' * Ensured folder exists: {folder}')
+    except OSError as e:
+        logger.error(f' * Failed to ensure folder: {folder} {e}')
+        raise
 
+# Simple helper function
+def dir_to_list(dir: str) -> list:
+    if not os.path.exists(dir):
+        return []
+    return os.listdir(dir)
 
-@app.route("/", methods=["GET"])
+# Route to index function, loads file names from downloads folder.
+@app.route('/', methods=['GET'])
 def index():
     downloads = dir_to_list(downloads_dir_path)
-    return render_template("index.html", downloads=downloads)
+    return render_template('index.html', downloads=downloads)
 
-@app.route("/scrape_form", methods=["POST"])
+# Route to post url input by the user, sent to scrape
+@app.route('/scrape_form', methods=['POST'])
 def scrape_form():
     try:
-        url_to_scrape = request.form.get("url", "").strip()
+        url_to_scrape = request.form.get('url', '').strip()
         if not url_to_scrape:
-            raise ValueError("No URL provided")
-        
+            raise ValueError('No URL provided')
+
         # Placeholder for scraping function scrape(url_to_scrape)
-        print("Scraping:", url_to_scrape)
-        print("Scraped:", url_to_scrape)
-        print("Images saved to downloads folder.")
+        logger.info(f' * Scraping: {url_to_scrape}')
+        logger.info(f' * Scraped: {url_to_scrape}')
+        logger.info(' * Images saved to downloads folder.')
 
+    # Placeholder error handling
     except ValueError as e:
-        print("Error:", e)
+        logger.error(f'Input error: {e}')
 
-    return redirect(url_for("index"))
+    # Reload index after url is sent.
+    return redirect(url_for('index'))
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
+    ensure_folder(downloads_dir_path)
     app.run(debug=True, port=8000)
