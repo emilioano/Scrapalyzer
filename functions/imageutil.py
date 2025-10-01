df2dd31cedefb51b847442aa4df01733f8e4f0df
emilioano <emil.sjoekvist@gmail.com>
2025-10-01 11:32:42 +0200

    Image handler function wip

--- functions/imageutil.py
+++ functions/imageutil.py
@@ -5,12 +5,10 @@ import cv2
 import torch
 import numpy as np
 
-
 from torchvision.models.detection import maskrcnn_resnet50_fpn
 from torchvision.models.detection.mask_rcnn import MaskRCNN_ResNet50_FPN_Weights
 from torchvision.transforms import functional as F
-
-
+from torchvision.ops import nms
 
 
 import urllib3
@@ -61,7 +59,6 @@ def imageprocessor(id=None):
         print(f'Loaded Image {file} Width is {width}, height is {height}')
         
         # Object segmentation
-        
         image_rgb = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
         image_tensor = F.to_tensor(image_rgb).unsqueeze(0)
 
@@ -69,12 +66,12 @@ def imageprocessor(id=None):
         with torch.no_grad():
             prediction = model(image_tensor)[0]
 
-        # Detection of objects in image
-        object_counter=0
+        # Defining of objects in image
+        object_counter=1
         for i in range(len(prediction['scores'])):
-            if prediction['scores'][i] > 0.8:
+            if prediction['scores'][i] > 0.80:
                 mask = prediction["masks"][i, 0].cpu().numpy()
-                mask_binary = mask > 0.5
+                mask_binary = mask > 0.5                
         
             # Creating new image with segmentet object only
             object = np.zeros_like(image_rgb)
@@ -94,37 +91,39 @@ def imageprocessor(id=None):
                 #Save object as separate image
                 object_bgr = cv2.cvtColor(object_crop, cv2.COLOR_RGB2BGR)
 
+                #Adding identifier to file name in case 'id' is inserted as argument
                 if id is None:
                     full_processed_path = processed_folder+'object_'+str(object_counter)+'_'+file
                 else:
                     full_processed_path = processed_folder+id+'_'+'object_'+str(object_counter)+'_'+file
 
-                
-
+                #Resize the image in case it's larger than predefined size in pixels
                 if object_width > size or object_height > size:
                     if object_width >= object_height:
                         calculate_height=int(object_height/object_width*size)
                         print(f'Object {object_counter} height: {object_height}, object width: {object_width}. Calculate height: {calculate_height}')
                         object_resized = cv2.resize(object_bgr, (size,calculate_height))
                         print(f'Object {object_counter}. Detected horinsontally aligned cropped image with size larger than {size} pixels, adjusted to width: {size} and height: {calculate_height}.')
-                    elif object_height >= object_width:
-                        calculate_width=int(width/height*size)
+                    else:
+                        calculate_width=int(object_width/object_height*size)
                         object_resized = cv2.resize(object_bgr, (calculate_width,size))
-                        print(f'Object {object_counter}. Detected vertically aliged cropped image detectred larger than {size} pixels, adjusted to width: {calculate_width} and height {size}.')
+                        print(f'Object {object_counter}. Detected vertically aligned cropped image detected larger than {size} pixels, adjusted to width: {calculate_width} and height {size}.')
                 else:
                     object_resized=object_bgr
 
                 save = cv2.imwrite(full_processed_path,object_resized)
 
                 if save:
-                    print(f'Image {full_processed_path} saved')
-                    #os.remove(full_download_path)
-                    #print(f'Image {full_download_path} removed')
+                    print(f'Object {object_counter}. Image {full_processed_path} saved')
+
                 else:
                     print(f'No image saved in {full_processed_path}')
 
                 object_counter+=1
 
+            #Clean download folder
+            #os.remove(full_download_path)
+            #print(f'Image {full_download_path} removed')
         
 '''
         if width >= height:
