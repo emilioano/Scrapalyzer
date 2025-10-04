5359a4b3fc9120c404562c18d31b2ccc60f06a78
gonzycodes <nahuel.gonzalezj@hotmail.com>
2025-10-04 17:57:34 +0200

    imported all necessary libraries. defined url, page and soup variables and also added fetch_image function with a for loop

---
+++ services/scraper/scraper.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+
+url = 'https://pixabay.com/sv/images/search/animal/'
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+
+def fetch_image():
+    images = soup.find_all('img')
+    for img in images:
+        print(img.get('src'))
