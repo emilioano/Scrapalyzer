15566d8e5f61f41c88df814b2bbcf632c58ac97c
gonzycodes <nahuel.gonzalezj@hotmail.com>
2025-10-04 20:37:19 +0200

    added comments to my lines

--- services/scraper/scraper.py
+++ services/scraper/scraper.py
@@ -9,7 +9,14 @@ url = 'https://pixabay.com/sv/images/search/animal/'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
 
+#Function for fetching image from url
 def fetch_image():
     images = soup.find_all('img')
     for img in images:
         print(img.get('src'))
+
+
+#Function for extracting image
+
+
+#Function for saving image
