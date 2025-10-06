6523efd9193b0d4cd1bec8f25b6529afe7908d61
gonzycodes <nahuel.gonzalezj@hotmail.com>
2025-10-06 16:39:01 +0200

    Rebuilt the code so the functions are inside a class to make it more user-friendly

--- services/scraper/scraper.py
+++ services/scraper/scraper.py
@@ -5,18 +5,34 @@ import requests
 from bs4 import BeautifulSoup
 from PIL import Image
 
-url = 'https://pixabay.com/sv/images/search/animal/'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
+#Class for the scraper function
+class Scraper:
+  #The constructor for the whole program
+  def __init__(self, url):
+      self.url = url
+      self.urls = []
+      self.extracted = []
+    
+#Function for fetching images from given URL
+  def fetch_image(self):
+     page = requests.get(self.url)
+     soup = BeautifulSoup(page.text,"html.parser")
+     images = soup.find_all('img')
+     for img in images:
+        self.urls.append(img.get('src'))
+     return self.urls
 
-#Function for fetching image from url
-def fetch_image():
-    images = soup.find_all('img')
-    for img in images:
-        print(img.get('src'))
 
 
 #Function for extracting image
+  def extract_image(self):
+     
+    
 
 
-#Function for saving image
+
+
+
+
+
+#Function for saving image to /downloads
