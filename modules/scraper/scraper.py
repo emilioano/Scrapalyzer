03930d87a50f9147e6699d8e93ec63253f4c34d3
gonzycodes <nahuel.gonzalezj@hotmail.com>
2025-10-11 10:19:06 +0200

    Completed a Pytest function, added headers variable that ties in with request

--- modules/scraper/scraper.py
+++ modules/scraper/scraper.py
@@ -1,10 +1,20 @@
-from urllib.parse import urljoin, urlparse
 import os
 from io import BytesIO
+from urllib.parse import urljoin, urlparse
 
 import requests
 from bs4 import BeautifulSoup
 from PIL import Image
+
+#Variables to simulate a browser to bypass bot blockers on certain websites
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://www.google.com",
+    "Connection": "keep-alive" 
+   
+   
+}
 
 
 #Class for the scraper function
@@ -18,12 +28,14 @@ class Scraper:
 #Function for fetching images from given URL
    def fetch_image(self):
      #Assigning values to variables so its easier to use bs4
-     page = requests.get(self.url)
+     page = requests.get(self.url, headers=headers)
      soup = BeautifulSoup(page.text,"html.parser")
      images = soup.find_all('img')
      #Going through every img tag, grabbing its 'src' and adding it to my self.urls list
      for img in images:
-        self.urls.append(img.get('src'))
+        src = img.get('src') or img.get('data-src')
+        if src:
+           self.urls.append(src)
      return self.urls
 
 
@@ -61,7 +73,7 @@ class Scraper:
        print('Failed to create directory!')
     #Looping through each link, adding different file-numbers for each file and also IF the specific url is too slow then it skips it
     for link in self.extracted:
-        response = requests.get(link, timeout=10)
+        response = requests.get(link, timeout=100, headers=headers)
         filename = link.split('/') [-1]
         save_path = os.path.join('data/downloads', filename)
         #If the response is successful, we save the image to the specified path
