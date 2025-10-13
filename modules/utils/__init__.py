e3716d13a9c0a8de0291d8bbcc77aff367c05ebf
emilioano <emil.sjoekvist@gmail.com>
2025-10-13 10:37:19 +0200

    init.py

---
+++ modules/utils/__init__.py
@@ -0,0 +1,3 @@
+from modules.utils.imageutil import imageprocessor,downloadanalyzermodel
+from modules.utils.imageremover import imageremover, RecursiveCleaner, FlatCleaner
+__all__ = ['imageprocessor','downloadanalyzermodel','imageremover','RecursiveCleaner','FlatCleaner']
