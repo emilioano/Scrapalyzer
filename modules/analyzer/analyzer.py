482907e66af1940e25bb48de4bbfeb26d29233fd
unknown <robinbegu@gmail.com>
2025-10-13 15:06:14 +0200

    clarified descriptions of analyzer class

--- modules/analyzer/analyzer.py
+++ modules/analyzer/analyzer.py
@@ -1,61 +1,39 @@
-import os
 
+#import for filesystem
+import os
+#imports for filefunctions, libraries for pics, modules, pytorch for tensor/ML
 import shutil
 from transformers import AutoModelForImageClassification, AutoImageProcessor
 from PIL import Image
 import torch
 
-
-
-
+#defines map for analyzed pics, creates if it doesnt exist, submap for pics without match
 analyzed_path = 'data/analyzed/'
 os.makedirs(analyzed_path, exist_ok=True)
 
 nomatch_path = 'data/analyzed/zero_matches/'
 
 
-
-
-
 class ImageAnalyzer:
-    '''
-    def init(self, model_name=""):
-        """test"""
-        pass
     
-    def analyze_images(self, analysed_dir, cats_dir, dogs_dir):
-        """analysed results in cats or dogs"""
-        pass
-
-    def classify_image(self, image_path):
-        """Classify a single image and return the predicted label and confidence."""
-        pass
-    '''
-    
+    #constructor
     def __init__(self, model_name=""):
-        """
-        Initialize the analyzer with a pre-trained model from Hugging Face.
-        Args:
-            model_name (str): Name of the pre-trained model from Hugging Face.
-        """
 
+        #load pretrained model from huggingface, load picprocesser, deciding GPU / CPU
         self.model = AutoModelForImageClassification.from_pretrained('modules/analyzer/models/vit-base-patch16-224',local_files_only=True)
         self.processor = AutoImageProcessor.from_pretrained('modules/analyzer/models/vit-base-patch16-224',local_files_only=True)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        #moving model to chosen device and puts in evaluation-state
         self.model.to(self.device)
         self.model.eval()
 
 
     
-
+    #defining method, path as input
     def classify_image(self, image_path):
-        """
-        Classify a single image and return classification.
-        Args:
-            image_path (str): Path to the image file.
-        Returns:
-            dict: Dictionary with predicted classification or potential error
-        """
+
+        #open pic, convert to RGB for consitency, pytorch-tensor analyze pic w processor
+        #moves tensor to correct device, runs pic to receive logit values, extracts logits
         try:
             image = Image.open(image_path).convert("RGB")
             inputs = self.processor(images=image, return_tensors="pt")
@@ -63,63 +41,59 @@ class ImageAnalyzer:
             with torch.no_grad():
                 outputs = self.model(**inputs)
                 logits = outputs.logits
+            #convert to probabilities, highest confidence w index, mapping index, extracting confidencevalue    
             probabilities = torch.softmax(logits, dim=1)
             top_prob, top_idx = torch.max(probabilities, dim=1)
             predicted_label = self.model.config.id2label[top_idx.item()]
             confidence = top_prob.item()
+            #returning results as dictionary
             print(f'Predicted label: {predicted_label}')
             return {
                 "label": predicted_label,
                 #confidence": confidence,
                 "error": None
             }
+        #errormessage if invalid
         except Exception as e:
             return {
                 "label": None,
                 #"confidence": None,
                 "error": f"Error classifying image {image_path}: {str(e)}"
             }
-
+    #defining method which analyzes directory and sorts pictures
     def analyze_images(self, analysed_dir, keywords):
+        #starting message
         print('Starting analyze_images')
-        """
-        Analyze all images in analysed_dir and move them to cats_dir or dogs_dir.
-        Args:
-            analysed_dir (str): Directory with analysed images.
-            cats_dir (str): Directory to store cat images.
-            dogs_dir (str): Directory to store dog images.
-        Returns:
-            list: List of dictionaries with classification results.
-        """
 
-                        
-
-
+        #initializing a list to save results
         results = []
         nokeyword = False
 
+        #looping through all files in directory, building searchable files
         for filename in os.listdir(analysed_dir):
             print(f'Filename: {filename}')
             if filename.lower().endswith(('.png', '.jpg', '.jpeg','.webp')):
                 image_path = os.path.join(analysed_dir, filename)
                 print(f'Image path: {image_path}')
+                #call classifying method, put result in list
                 result = self.classify_image(image_path)
                 result["filename"] = filename
                 results.append(result)
 
+                #checking if there is no error, get raw label, replacing "_" and " "
                 if result.get("error") is None:
                     labelraw = result.get("label", "").lower()
                     label = labelraw.replace(',',' -')
+                    #printing classification and path
                     print(f'Identification done in in {filename}, object is identified as {label}!')
                               
-                    
+                    #assigning keywords if label is empty, with a message
                     if not keywords:
                         keywords = [label.replace(',',' -')]
                         nokeyword = True
                         print(f'Keyword was empty, keyword is now {keywords}')
                     
-
-                    #Moving file to corresponding folder based on keyword.
+                    #looping through keywords, moving file to corresponding folder based on keyword.
                     for keyword in keywords:
                         fullpath=os.path.join(analyzed_path, keyword)
 
@@ -131,11 +105,11 @@ class ImageAnalyzer:
                             print(f'Keyword {keyword} was found in label {label}. Moved to {fullpath}{filename}')
                         else:
                             print(f'Keyword {keyword} not found in the label: {label}')
-
+                    #nullify keywords if generated
                     if nokeyword:
                         keywords = []
 
-        #Moving the files that are left to nomatch_path
+        #moving the files that are left to nomatch_path
         for filename in os.listdir(analysed_dir):
             os.makedirs(nomatch_path, exist_ok=True)
             print(f'Folder {nomatch_path} for no matched object exists or is now created!')
